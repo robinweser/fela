11270447a50b9a00b16b2c468b21961b4b80b2eb
rofrischmann <robin@rofrischmann.de>
2016-05-25 09:29:36 +0200

    added basic font component

---
+++ modules/components/dom/Font.js
@@ -0,0 +1,37 @@
+const formats = {
+  '.woff': 'woff',
+  '.eof': 'eof',
+  '.ttf': 'truetype',
+  '.svg': 'svg'
+}
+
+// Returns the font format for a specific font source
+function getFontFormat(src) {
+  return Object.keys(formats).reduce((format, extension) => {
+    if (src.indexOf(extension) > -1) {
+      format = formats[extension]
+    }
+    return format; // eslint-disable-line
+  }, undefined)
+}
+
+
+export default class Font {
+  constructor(family, files, properties = { }) {
+    this.family = family
+    this.files = files
+    this.properties = properties
+  }
+
+  render() {
+    const font = {
+      fontFamily: `'${fontFamily}'`,
+      src: files.map(src => `url('${src}') format('${getFontFormat(src)}')`).join(',')
+    }
+
+    const fontProperties = [ 'fontWeight', 'fontStretch', 'fontStyle', 'unicodeRange' ]
+    Object.keys(properties).filter(prop => fontProperties.indexOf(prop) > -1).forEach(fontProp => font[fontProp] = properties[fontProp])
+
+    return font
+  }
+}
