dccf1c20a3126a6665cffc9a6157971f8548d7ee
rofrischmann <robin@rofrischmann.de>
2016-06-08 08:14:08 +0200

    simplified combineSelectors

--- modules/combineSelectors.js
+++ modules/combineSelectors.js
@@ -1,10 +1,5 @@
 import deepAssign from 'deep-assign'
 
 export default function combineSelectors(...selectors) {
-  return props => {
-    const style = { }
-
-    selectors.forEach(selector => deepAssign(style, selector(props)))
-    return style
-  }
+  return props => selectors.reduce((style, selector) => deepAssign(style, selector(props)), { })
 }
