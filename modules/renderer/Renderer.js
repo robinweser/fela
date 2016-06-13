91379f0d3a16d16ece3c99eff51cb8e654af27d5
Johannes Lumpe <johannes@johanneslumpe.de>
2016-06-13 17:30:36 +0300

    Revert "`properties` -> `props` for more consistency"
    This reverts commit 1531209500e0264a815ee1249463a0201fbf28e8.

--- modules/renderer/Renderer.js
+++ modules/renderer/Renderer.js
@@ -114,7 +114,7 @@ export default function Renderer(config = { }) {
      * @param {FontFace} fontFace - fontFace which gets rendered
      * @return {string} fontFamily reference
      */
-    renderFont(family, files, props = { }) {
+    renderFont(family, files, properties = { }) {
       if (!r.rendered.hasOwnProperty(family)) {
         const fontFace = {
           fontFamily: '\'' + family + '\'',
@@ -122,7 +122,7 @@ export default function Renderer(config = { }) {
         }
 
         const fontProperties = [ 'fontVariant', 'fontWeight', 'fontStretch', 'fontStyle', 'unicodeRange' ]
-        Object.keys(props).filter(prop => fontProperties.indexOf(prop) > -1).forEach(fontProp => fontFace[fontProp] = props[fontProp])
+        Object.keys(properties).filter(prop => fontProperties.indexOf(prop) > -1).forEach(fontProp => fontFace[fontProp] = properties[fontProp])
 
         const css = '@font-face{' + cssifyObject(fontFace) + '}'
         r.rendered[family] = true
