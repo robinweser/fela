857b201972b08cbc5a814ee2cbcffd059bb0ce83
rofrischmann <robin@rofrischmann.de>
2016-06-21 12:20:08 +0200

    #44 initial validator plugin

---
+++ modules/plugins/validator.js
@@ -0,0 +1,28 @@
+function validator(style, options) {
+  const { logInvalid, deleteInvalid } = options
+
+  Object.keys(style).forEach(property => {
+    const value = style[property]
+    if (value === undefined || (typeof value === 'string' && value.indexOf('undefined') > -1)) {
+      if (deleteInvalid) {
+        delete style[property]
+      }
+      if (logInvalid) {
+        console.log((deleteInvalid ? '[Deleted] ' : ' ') + 'Invalid Property', {
+          property,
+          value
+        })
+      }
+    } else if (value instanceof Object && !Array.isArray(value)) {
+      style[property] = validator(value, options)
+    }
+  })
+
+  return style
+}
+
+const defaultOptions = { logInvalid: true, deleteInvalid: false }
+export default (options) => style => validator(style, {
+  ...defaultOptions,
+  ...options
+})
