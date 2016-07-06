6cc576cd7dedb92cc19aeb82f3fb57ad145db35f
rofrischmann <robin@rofrischmann.de>
2016-07-06 17:17:09 +0200

    #62 added dependencies option to build script
    closes #62

--- scripts/buildPackages.js
+++ scripts/buildPackages.js
@@ -4,13 +4,71 @@ import uglify from 'rollup-plugin-uglify'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolver from 'rollup-plugin-node-resolve'
 
-import packages from '../packages'
-
-// Small helper to error and exit on fail
-const errorOnFail = err => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
+const packages = {
+  fela: {
+    name: 'Fela',
+    entry: 'index.js',
+    dependencies: true
+  },
+  'fela-plugin-extend': {
+    name: 'FelaPluginExtend',
+    entry: 'plugins/extend.js',
+    dependencies: false
+  },
+  'fela-plugin-dynamic-prefixer': {
+    name: 'FelaPluginDynamicPrefixer',
+    entry: 'plugins/dynamicPrefixer.js',
+    dependencies: true
+  },
+  'fela-plugin-prefixer': {
+    name: 'FelaPluginPrefixer',
+    entry: 'plugins/prefixer.js',
+    dependencies: true
+  },
+  'fela-plugin-fallback-value': {
+    name: 'FelaPluginFallbackValue',
+    entry: 'plugins/fallbackValue.js',
+    dependencies: true
+  },
+  'fela-plugin-custom-property': {
+    name: 'FelaPluginCustomProperty',
+    entry: 'plugins/customProperty.js',
+    dependencies: false
+  },
+  'fela-plugin-friendly-pseudo-class': {
+    name: 'FelaPluginFriendlyPseudoClass',
+    entry: 'plugins/friendlyPseudoClass.js',
+    dependencies: false
+  },
+  'fela-plugin-unit': {
+    name: 'FelaPluginUnit',
+    entry: 'plugins/unit.js',
+    dependencies: true
+  },
+  'fela-plugin-lvha': {
+    name: 'FelaPluginLVHA',
+    entry: 'plugins/LVHA.js',
+    dependencies: false
+  },
+  'fela-plugin-logger': {
+    name: 'FelaPluginLogger',
+    entry: 'plugins/logger.js',
+    dependencies: false
+  },
+  'fela-plugin-validator': {
+    name: 'FelaPluginValidator',
+    entry: 'plugins/validator.js',
+    dependencies: false
+  },
+  'fela-beautifier': {
+    name: 'FelaBeautifier',
+    entry: 'enhancers/beautifier.js',
+    dependencies: true
+  },
+  'fela-perf': {
+    name: 'FelaPerf',
+    entry: 'enhancers/perf.js',
+    dependencies: false
   }
 }
 
@@ -23,9 +81,8 @@ const nodeResolverPlugin = nodeResolver({ jsnext: true, main: true })
 const commonJSPlugin = commonjs({ include: 'node_modules/**' })
 const uglifyPlugin = uglify()
 
-const plugins = [ babelPlugin, nodeResolverPlugin, commonJSPlugin ]
-
 function rollupConfig(pkg, info, minify) {
+  const plugins = info.dependencies ? [ babelPlugin, nodeResolverPlugin, commonJSPlugin ] : [ babelPlugin ]
   return {
     entry: 'modules/' + info.entry,
     plugins: minify ? plugins.concat(uglifyPlugin) : plugins
@@ -45,7 +102,10 @@ function buildPackage(pkg) {
   rollup.rollup(rollupConfig(pkg, packages[pkg], process.env.NODE_ENV === 'production')).then(bundle => {
     bundle.write(bundleConfig(pkg, packages[pkg], process.env.NODE_ENV === 'production'))
     console.log('Successfully bundled ' + packages[pkg].name + (process.env.NODE_ENV === 'production' ? ' (minified).' : '.'))
-  }).catch(errorOnFail)
+  }).catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
 }
 
 Object.keys(packages).forEach(pkg => buildPackage(pkg))
