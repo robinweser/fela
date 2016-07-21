bd7e9271855fb56c6f6437965c7e2b6c34968c9f
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-07-21 13:05:15 +0200

    optional param for createRenderer

--- packages/fela/index.d.ts
+++ packages/fela/index.d.ts
@@ -1,4 +1,4 @@
-declare function createRenderer (config: Object): Object;
+declare function createRenderer (config?: Object): Object;
 declare function combineRules (...rules: Object[]): Function;
 declare function enhance (...enhancers: Object[]): Function;
 declare function render (renderer: Object, mountNode: Object): void;
