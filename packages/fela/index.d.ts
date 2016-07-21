341eecfabf7622890ef3f7f86f763818e49ac729
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-07-21 13:30:34 +0200

    fix exports

--- packages/fela/index.d.ts
+++ packages/fela/index.d.ts
@@ -1,4 +1,4 @@
-declare function createRenderer (config?: Object): Object;
-declare function combineRules (...rules: Object[]): Function;
-declare function enhance (...enhancers: Object[]): Function;
-declare function render (renderer: Object, mountNode: Object): void;
+export declare function createRenderer(config?:Object): Object;
+export declare function combineRules(...rules:Object[]): Function;
+export declare function enhance(...enhancers:Object[]): Function;
+export declare function render(renderer:Object, mountNode:Object): void;
