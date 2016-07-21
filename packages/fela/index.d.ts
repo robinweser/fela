1a1855a9b8ea925da8ac8b5278ce01c5d3b4298a
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-07-21 17:06:36 +0200

    Typings for Renderer finished

--- packages/fela/index.d.ts
+++ packages/fela/index.d.ts
@@ -1,9 +1,14 @@
-/* TODO: Add all Interfaces and Methods */
-interface Renderer {
-    renderRule(rule: Function, props?: any): string
+export interface Renderer {
+    clear(): void;
+    renderRule(rule: Function, props?: any): string;
+    renderKeyframe(keyframe: any, props?: any): string;
+    renderFont(family: string, files: any[], properties?: any): string;
+    renderStatic(style: string | any, selector?: string): string;
+    renderToString(): string;
+    subscribe(callback: Function): any;
 }
 
 export declare function createRenderer(config?:any): Renderer;
 export declare function combineRules(...rules:any[]): Function;
 export declare function enhance(...enhancers:any[]): Function;
-export declare function render(renderer:any, mountNode:HTMLElement): void;
+export declare function render(renderer:Renderer, mountNode:HTMLElement): void;
