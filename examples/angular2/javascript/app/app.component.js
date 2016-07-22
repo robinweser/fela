da6083f41f6e565e11332a5cb9b9ac00eb684093
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-07-22 16:18:01 +0200

    Angular 2 ES6 JS example - example project working with fela

--- examples/angular2/javascript/app/app.component.js
+++ examples/angular2/javascript/app/app.component.js
@@ -1,17 +1,30 @@
-import { Component, Inject } from '@angular/core';
-import FelaRendererService from './felaRenderer.service';
+import { Component } from '@angular/core';
+import { FelaRendererService } from './felaRenderer.service';
+import template from './app.component.html';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <h1>Foo</h1>
-    `
-
+    template: template
 })
 export class AppComponent {
 
-    constructor() {
+    constructor(felaRendererService: FelaRendererService) {
+        this.felaRendererService = felaRendererService;
+    }
 
+    initFelaStyles() {
+        const rule = props => ({
+            fontSize: props.fontSize + 'px',
+            color: props.color ? props.color : 'red',
+        });
+
+        this.headlineClass = this.felaRendererService.renderRule(rule, { fontSize: 20 });
+        this.infoBoxClass = this.felaRendererService.renderRule(rule, { color: '#00ff00' });
+    }
+
+    ngOnInit() {
+        this.initFelaStyles();
     }
 
 }
+
--- examples/angular2/javascript/app/felaRenderer.service.js
+++ examples/angular2/javascript/app/felaRenderer.service.js
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { createRenderer, render } from 'fela';
 
+@Injectable()
 export class FelaRendererService {
     static renderer = null;
 
