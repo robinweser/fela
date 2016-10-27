87ba79d0ccea42f2058977de1295830433339713
rofrischmann <robin@rofrischmann.de>
2016-10-27 20:26:01 +0200

    #95 added a plugin to prefix placeholder prefixes

---
+++ modules/plugins/placeholderPrefixer.js
@@ -0,0 +1,10 @@
+import customProperty from './customProperty'
+
+const placeholderPrefixes = [ '::-webkit-input-placeholder', '::-moz-placeholder', ':-ms-input-placeholder', ':-moz-placeholder', '::placeholder' ]
+
+export default () => customProperty({
+  '::placeholder': value => placeholderPrefixes.reduce((style, prefix) => {
+    style[prefix] = value
+    return style
+  }, { })
+})
