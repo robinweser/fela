78b3beddbef041d4045e4e99b593ffed8c17410a
rofrischmann <robin@rofrischmann.de>
2016-11-26 12:51:20 +0100

    #84 overwriting ability

--- modules/bindings/react/ThemeProvider.js
+++ modules/bindings/react/ThemeProvider.js
@@ -1,14 +1,15 @@
 import { Component, PropTypes } from 'react'
 
-export default class Theme extends Component {
-  static propTypes =  { theme: PropTypes.object };
-  static childContextTypes = { theme: PropTypes.object };
-  static contextTypes = { theme: PropTypes.object };
+export default class ThemeProvider extends Component {
+  static propTypes =  { theme: PropTypes.object, flat: PropTypes.bool, overwrite: PropTypes.bool };
+  static childContextTypes = { theme: PropTypes.object, flat: PropTypes.bool };
+  static contextTypes = { theme: PropTypes.object, flat: PropTypes.bool };
 
   getChildContext() {
     return {
+      flat: this.props.flat || this.props.flat,
       theme: {
-        ...this.context.theme,
+        ...(!overwrite && this.context.theme),
         ...this.props.theme
       }
     }
