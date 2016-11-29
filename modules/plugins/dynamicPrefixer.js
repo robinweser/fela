cf919609b9f1ed0cf61f66cc279c40cfa8d5e83d
Cavitt <cavitt.glover@gmail.com>
2016-11-29 15:03:54 -0600

    Memoize prefixing plugins
    If the same style object is passed to a Prefix instance, a cached value
    is returned. A WeakMap is used so the cache does not aggressively hold
    on to referenced style objects.

--- modules/plugins/dynamicPrefixer.js
+++ modules/plugins/dynamicPrefixer.js
@@ -3,6 +3,13 @@ import Prefixer from 'inline-style-prefixer'
 
 export default options => {
   const prefixer = new Prefixer(options)
-
-  return style => prefixer.prefix(style)
+  const index = new WeakMap()
+  return style => {
+    let ret = index.get(style)
+    if (!ret) {
+      ret = prefixer.prefix(style)
+      index.set(style, ret)
+    }
+    return ret
+  }
 }
--- modules/plugins/prefixer.js
+++ modules/plugins/prefixer.js
@@ -1,4 +1,14 @@
 /* @flow weak */
 import prefix from 'inline-style-prefixer/static'
 
-export default () => style => prefix(style)
+export default () => {
+  const index = new WeakMap()
+  return style => {
+    let ret = index.get(style)
+    if (!ret) {
+      ret = prefix(style)
+      index.set(style, ret)
+    }
+    return ret
+  }
+}
