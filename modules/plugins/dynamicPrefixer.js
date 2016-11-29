ff82c4333a3d0e6b069fb9b785f7f987f2000290
Cavitt <cavitt.glover@gmail.com>
2016-11-29 16:04:19 -0600

    Remove prefixing memoization, only renderRule needed

--- modules/plugins/dynamicPrefixer.js
+++ modules/plugins/dynamicPrefixer.js
@@ -3,16 +3,6 @@ import Prefixer from 'inline-style-prefixer'
 
 export default options => {
   const prefixer = new Prefixer(options)
-  const weakMapSupported = ('WeakMap' in global)
-  const index = weakMapSupported && new WeakMap()
-  return style => {
-    let ret = index && index.get(style)
-    if (!ret) {
-      ret = prefixer.prefix(style)
-      if (index) {
-        index.set(style, ret)
-      }
-    }
-    return ret
-  }
+
+  return style => prefixer.prefix(style)
 }
--- modules/plugins/prefixer.js
+++ modules/plugins/prefixer.js
@@ -1,16 +1,4 @@
 /* @flow weak */
 import prefix from 'inline-style-prefixer/static'
 
-export default () => {
-  const index = ('WeakMap' in global) && new WeakMap()
-  return style => {
-    let ret = index && index.get(style)
-    if (!ret) {
-      ret = prefix(style)
-      if (index) {
-        index.set(style, ret)
-      }
-    }
-    return ret
-  }
-}
+export default () => style => prefix(style)
