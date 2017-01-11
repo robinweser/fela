cc541cdff553462127079974f80dd0ef9dc9f193
rofrischmann <robin@rofrischmann.de>
2017-01-11 08:17:55 +0100

    #176 removed uppercase letters in classnames
    closes #176

--- modules/utils/generateClassName.js
+++ modules/utils/generateClassName.js
@@ -1,11 +1,12 @@
 /* @flow weak */
-const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const chars = 'abcdefghijklmnopqrstuvwxyz'
+const charLength = chars.length
 
 export default function generateClassName(id, className = '') {
-  if (id <= 52) {
+  if (id <= charLength) {
     return chars[id - 1] + className
   }
 
   // Bitwise floor as safari performs much faster https://jsperf.com/math-floor-vs-math-round-vs-parseint/55
-  return generateClassName(id / 52 | 0, chars[id % 52] + className)
+  return generateClassName(id / charLength | 0, chars[id % charLength] + className)
 }
