24ac5ecac78be4fbbf64895e34ed35c6e09c86da
rofrischmann <robin@rofrischmann.de>
2017-01-11 08:19:44 +0100

    fix tests

--- test/utils/generateClassName-test.js
+++ test/utils/generateClassName-test.js
@@ -4,8 +4,8 @@ describe('Generating a className', () => {
   it('should return a unique className', () => {
     expect(generateClassName(1)).to.eql('a')
     expect(generateClassName(2)).to.eql('b')
-    expect(generateClassName(53)).to.eql('ab')
-    expect(generateClassName(54)).to.eql('ac')
+    expect(generateClassName(53)).to.eql('bb')
+    expect(generateClassName(54)).to.eql('bc')
   })
 
   it('should return the same className', () => {
