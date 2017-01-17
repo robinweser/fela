40904ea528091d095ed6ad7daa5dc0d5a4298fa6
Benjamin Kniffler <bkniffler@me.com>
2017-01-17 13:48:46 +0000

    let react-fela passThroughProps accept function
    This is untested, gave editing via github website a try :)
    Going to test later.
    
    Related to https://github.com/rofrischmann/fela/issues/174

--- modules/bindings/react/createComponent.js
+++ modules/bindings/react/createComponent.js
@@ -1,15 +1,27 @@
 /* @flow weak */
 import { createElement, PropTypes } from 'react'
 
+// Extract props by either an array ['key1', 'key2'] or a function props => { key1: props.key1 }
+const getProps = (arrOrFunc, ruleProps) => {
+  const arrOrObj = arrOrFunc && typeof arrOrFunc === 'function' ? arrOrFunc(ruleProps) : arrOrFunc;
+  if (arrOrObj && Array.isArray(arrOrObj)) {
+    return arrOrObj.reduce((output, prop) => {
+      output[prop] = ruleProps[prop];
+      return output;
+    }, {});
+  }
+  else if (arrOrObj && typeof arrOrObj === 'object') {
+    return arrOrObj;
+  };
+  return {};
+};
+
 export default function createComponent(rule, type = 'div', passThroughProps = []) {
   const FelaComponent = ({ children, className, id, style, passThrough = [], ...ruleProps }, { renderer, theme }) => {
 
-    // filter props to extract props to pass through
-    const componentProps = [ ...passThroughProps, ...passThrough ].reduce((output, prop) => {
-      output[prop] = ruleProps[prop]
-      return output
-    }, { })
-
+    // compose props from arrays or functions
+    const componentProps = { ...getProps(passThroughProps), ...getProps(passThrough) };
+ 
     componentProps.style = style
     componentProps.id = id
 
