9fa502b10eb8b7af474d6d77f5cdc288a7b21ab4
Pavel Zika <pzika@langmaster.cz>
2017-02-27 14:12:25 +0100

    Better IRenderer.subscribe TS definition

--- packages/fela/index.d.ts
+++ packages/fela/index.d.ts
@@ -9,13 +9,30 @@ declare module "fela" {
   type TPlugin = (style: IStyle) => IStyle; //http://fela.js.org/docs/advanced/Plugins.html
   type TEnhancer = (renderer: IRenderer) => IRenderer; //http://fela.js.org/docs/advanced/Enhancers.html
 
+  const enum TSubscribeMessageType {
+    rule = 1,
+    staticObject = 1,
+    keyframes = 2,
+    fontFace = 3,
+    staticString = 4,
+    clear = 5
+  }
+  interface TSubscribeMessage {
+    type: TSubscribeMessageType;
+  }
+  interface TSubscribeRuleMessage extends TSubscribeMessage { static?: boolean; declaration: string; selector: string; media: string; }
+  interface TSubscribeKeyframesMessage extends TSubscribeMessage { name: string; keyframe: string; }
+  interface TSubscribeFontFaceMessage extends TSubscribeMessage { fontFamily: string; fontFace: string; }
+  interface TSubscribeStaticStringMessage extends TSubscribeMessage { css: string; }
+  interface TSubscribeClearMessage extends TSubscribeMessage { }
+
   interface IRenderer {
     renderRule(rule: TRule, props: TRuleProps): void;
     renderKeyframe(keyFrame: TKeyFrame, props: TRuleProps): void;
     renderFont(family: string, files: Array<string>, props: TRuleProps): void;
     renderStatic(style: string, selector?: string): void;
     renderToString(): string;
-    subscribe(event: (msg: string) => void): { unsubscribe: () => void; }
+    subscribe(event: (msg: TSubscribeRuleMessage | TSubscribeKeyframesMessage | TSubscribeFontFaceMessage | TSubscribeStaticStringMessage | TSubscribeClearMessage) => void): { unsubscribe: () => void; }
     clear();
   }
 
