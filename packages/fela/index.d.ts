566401158e092989b951e69bb655891872071a73
Pavel Zika <pzika@langmaster.cz>
2017-02-27 11:12:33 +0100

    Update index.d.ts

--- packages/fela/index.d.ts
+++ packages/fela/index.d.ts
@@ -1,13 +1,44 @@
-export interface Renderer {
-    clear(): void;
-    renderRule(rule: Function, props?: any): string;
-    renderKeyframe(keyframe: any, props?: any): string;
-    renderFont(family: string, files: any[], properties?: any): string;
-    renderStatic(style: string | any, selector?: string): string;
+declare module "fela" {
+
+  import React from 'react';
+
+  type TRuleProps = {};
+  type TRule = (props: TRuleProps) => IStyle; 
+  type TKeyFrame = TRule;
+  type TRendererCreator = (config?: IConfig) => IRenderer;
+  type TPlugin = (style: IStyle) => IStyle; //http://fela.js.org/docs/advanced/Plugins.html
+  type TEnhancer = (renderer: IRenderer) => IRenderer; //http://fela.js.org/docs/advanced/Enhancers.html
+
+  interface IRenderer {
+    renderRule(rule: TRule, props: TRuleProps): void;
+    renderKeyframe(keyFrame: TKeyFrame, props: TRuleProps): void;
+    renderFont(family: string, files: Array<string>, props: TRuleProps): void;
+    renderStatic(style: string, selector?: string): void;
     renderToString(): string;
-    subscribe(callback: Function): any;
+    subscribe(event: (msg: string) => void): { unsubscribe: () => void; }
+    clear();
+  }
+
+  //http://fela.js.org/docs/advanced/RendererConfiguration.html
+  interface IConfig {
+    plugins?: Array<TPlugin>;
+    keyframePrefixes?: Array<string>;
+    enhancers?: Array<TEnhancer>;
+    mediaQueryOrder?: Array<string>;
+    selectorPrefix?: string;
+  }
+
+  interface IStyle extends React.CSSProperties {
+    //TODO: add properties, missing in React.CSSProperties
+  }
+
+  function createRenderer(config?: IConfig): IRenderer;
+  function combineRules(...rules: Array<TRule>): TRule;
+  function enhance(...enhancers: Array<TEnhancer>): (rendererCreator: TRendererCreator) => TRendererCreator;
 }
 
-export declare function createRenderer(config?:any): Renderer;
-export declare function combineRules(...rules:any[]): Function;
-export declare function enhance(...enhancers:any[]): Function;
+declare module "fela-dom" {
+  import { IRenderer } from 'fela';
+  function render(renderer: IRenderer, node: HTMLElement);
+}
+
