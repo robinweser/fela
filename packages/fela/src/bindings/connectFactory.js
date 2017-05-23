0ad54a3b7b9b42548beffd066c283bf67e004e4e
Robin Frischmann <robin@rofrischmann.de>
2017-05-23 15:58:22 +0200

    backwards compatibility fallback for connect with deprecation warning

--- packages/fela/src/bindings/connectFactory.js
+++ packages/fela/src/bindings/connectFactory.js
@@ -7,26 +7,41 @@ export default function connectFactory(
   createElement: Function,
   contextTypes?: Object
 ): Function {
-  return function connect(rules: Object): Function {
+  return function connect(rules: Object | Function): Function {
     return (component: any): any => {
       class EnhancedComponent extends BaseComponent {
-        static displayName = generateDisplayName(component)
+        static displayName = generateDisplayName(component);
 
         render() {
-          const { renderer, theme = {} } = this.context
+          const { renderer, theme } = this.context
+
           const styleProps = {
             ...this.props,
-            theme
+            theme: theme || {}
           }
 
-          const styles = objectReduce(
-            rules,
-            (styleMap, rule) => {
-              styleMap[rule] = renderer.renderRule(rules[rule], styleProps)
-              return styleMap
-            },
-            {}
-          )
+          let styles
+
+          if (typeof rules === 'function') {
+            styles = rules(styleProps)(renderer)
+
+            // deprecation warning
+            if (process.env.NODE_ENV !== 'production' && !this.warnDeprecated) {
+              console.warn(
+                "Using a `mapStylesToProps` function with react-fela's `connect` is deprecated. It will be removed soon. Use a direct `rules` object. See https://github.com/rofrischmann/fela/blob/master/packages/react-fela/docs/connect.md"
+              )
+              this.warnDeprecated = true
+            }
+          } else {
+            styles = objectReduce(
+              rules,
+              (styleMap, rule) => {
+                styleMap[rule] = renderer.renderRule(rules[rule], styleProps)
+                return styleMap
+              },
+              {}
+            )
+          }
 
           return createElement(component, {
             ...this.props,
