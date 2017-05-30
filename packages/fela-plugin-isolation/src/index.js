4511804d10b4f93a0a3098ac02f4e85d09ba3a56
tajo <vojtech@miksu.cz>
2017-05-29 22:48:00 -0700

    unbundle fela-plugin-isolation

--- packages/fela-plugin-isolation/src/index.js
+++ packages/fela-plugin-isolation/src/index.js
@@ -1,1 +1,30 @@
-export * from 'fela/lib/plugins/isolation'
+/* @flow */
+import { arrayReduce } from 'fela-tools'
+
+function addIsolation(style: Object, exclude: Array<string> = []): Object {
+  if (style.isolation === false) {
+    // remove the isolation prop to
+    // prevent false CSS properties
+    delete style.isolation
+    return style
+  }
+
+  const excludedDeclarations = arrayReduce(
+    exclude,
+    (exclusion, property) => {
+      exclusion[property] = 'inherit'
+      return exclusion
+    },
+    {}
+  )
+
+  return {
+    all: 'initial',
+    ...excludedDeclarations,
+    ...style
+  }
+}
+
+export default function isolation(options: Object = {}) {
+  return (style: Object) => addIsolation(style, options.exclude)
+}
