8f362f29ef7d629e98f2140ded96a1243d18385a
tajo <vojtech@miksu.cz>
2017-05-29 22:55:39 -0700

    unbundle fela-plugin-placeholder-prefixer

---
+++ packages/fela-plugin-placeholder-prefixer/src/__tests__/placeholderPrefixer-test.js
@@ -0,0 +1,19 @@
+import placeholderPrefixer from '../index'
+
+describe('Placeholder prefixer plugin', () => {
+  it('should add placeholder prefixes', () => {
+    const style = {
+      width: 20,
+      '::placeholder': { color: 'red' }
+    }
+
+    expect(placeholderPrefixer()(style)).toEqual({
+      width: 20,
+      '::-webkit-input-placeholder': { color: 'red' },
+      '::-moz-placeholder': { color: 'red' },
+      ':-ms-input-placeholder': { color: 'red' },
+      ':-moz-placeholder': { color: 'red' },
+      '::placeholder': { color: 'red' }
+    })
+  })
+})
--- packages/fela-plugin-placeholder-prefixer/src/index.js
+++ packages/fela-plugin-placeholder-prefixer/src/index.js
@@ -1,1 +1,25 @@
-export * from 'fela/lib/plugins/placeholderPrefixer'
+/* @flow */
+import customProperty from 'fela-plugin-custom-property'
+import { arrayReduce } from 'fela-tools'
+
+const placeholderPrefixes = [
+  '::-webkit-input-placeholder',
+  '::-moz-placeholder',
+  ':-ms-input-placeholder',
+  ':-moz-placeholder',
+  '::placeholder'
+]
+
+export default function placeholderPrefixer() {
+  return customProperty({
+    '::placeholder': value =>
+      arrayReduce(
+        placeholderPrefixes,
+        (style, prefix) => {
+          style[prefix] = value
+          return style
+        },
+        {}
+      )
+  })
+}
