04089895114538c1101f8d8dbf0528f019ea1363
tajo <vojtech@miksu.cz>
2017-05-29 22:46:11 -0700

    unbundle fela-plugin-important

--- packages/fela-plugin-important/src/index.js
+++ packages/fela-plugin-important/src/index.js
@@ -1,1 +1,32 @@
-export * from 'fela/lib/plugins/important'
+/* @flow */
+import { isObject } from 'fela-tools'
+
+function addImportantToValue(value: any): any {
+  if (
+    typeof value === 'number' ||
+    (typeof value === 'string' &&
+      value.toLowerCase().indexOf('!important') === -1)
+  ) {
+    return `${value}!important`
+  }
+
+  return value
+}
+
+function addImportant(style: Object): Object {
+  for (const property in style) {
+    const value = style[property]
+
+    if (isObject(value)) {
+      style[property] = addImportant(value)
+    } else if (Array.isArray(value)) {
+      style[property] = value.map(addImportantToValue)
+    } else {
+      style[property] = addImportantToValue(value)
+    }
+  }
+
+  return style
+}
+
+export default () => addImportant
