4e77934c2004af317c94b9fbf6c68d7055327b1d
tajo <vojtech@miksu.cz>
2017-05-29 18:54:57 -0700

    unbundle fela-beautifier

--- packages/fela-beautifier/src/index.js
+++ packages/fela-beautifier/src/index.js
@@ -1,1 +1,29 @@
-export * from 'fela/lib/enhancers/beautifier'
+/* @flow */
+import cssbeautify from 'cssbeautify'
+
+import type DOMRenderer from '../../../flowtypes/DOMRenderer'
+
+function addBeautifier(renderer: DOMRenderer, options: Object): DOMRenderer {
+  const existingRenderToString = renderer.renderToString.bind(renderer)
+
+  renderer.renderToString = (): string => {
+    const css = existingRenderToString()
+    return cssbeautify(css, options)
+  }
+
+  return renderer
+}
+
+const defaultOptions = {
+  indent: '  ',
+  openbrace: 'end-of-line',
+  autosemicolon: false
+}
+
+export default function beautifier(options: Object = {}) {
+  return (renderer: DOMRenderer) =>
+    addBeautifier(renderer, {
+      ...defaultOptions,
+      ...options
+    })
+}
