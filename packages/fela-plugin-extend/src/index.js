181ee133a30b98da7967f74eeabc8013644abd9e
tajo <vojtech@miksu.cz>
2017-05-29 22:39:45 -0700

    unbundle fela-plugin-extend

--- packages/fela-plugin-extend/src/index.js
+++ packages/fela-plugin-extend/src/index.js
@@ -1,1 +1,40 @@
-export * from 'fela/lib/plugins/extend'
+/* @flow */
+import assignStyle from 'css-in-js-utils/lib/assignStyle'
+
+import { isObject, arrayEach } from 'fela-tools'
+
+function extendStyle(
+  style: Object,
+  extension: Object,
+  extendPlugin: Function
+): void {
+  // extend conditional style objects
+  if (extension.hasOwnProperty('condition')) {
+    if (extension.condition) {
+      assignStyle(style, extendPlugin(extension.style))
+    }
+  } else {
+    // extend basic style objects
+    assignStyle(style, extension)
+  }
+}
+
+function extend(style: Object): Object {
+  for (const property in style) {
+    const value = style[property]
+
+    if (property === 'extend') {
+      const extensions = [].concat(value)
+
+      arrayEach(extensions, extension => extendStyle(style, extension, extend))
+      delete style[property]
+    } else if (isObject(value)) {
+      // support nested extend as well
+      style[property] = extend(value)
+    }
+  }
+
+  return style
+}
+
+export default () => extend
