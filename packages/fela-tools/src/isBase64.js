31b845bcbe102e11e81ec4a3a121433c050927bd
Andrew Sun <me@andrewsun.com>
2017-06-03 16:23:32 -0400

    Optimize isBase64.js

--- packages/fela-tools/src/isBase64.js
+++ packages/fela-tools/src/isBase64.js
@@ -1,4 +1,8 @@
 /* @flow */
 export default function isBase64(property: string): boolean {
-  return property.indexOf('data:') !== -1
+  return property.charAt(0) === 'd'
+      && property.charAt(1) === 'a'
+      && property.charAt(2) === 't'
+      && property.charAt(3) === 'a'
+      && property.charAt(4) === ':'
 }
