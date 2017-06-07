05db143487c4b64d6f8d42496cb780780c306639
Robin Frischmann <robin@rofrischmann.de>
2017-06-07 17:31:28 +0200

    basic react native media query plugin

---
+++ packages/fela-native-plugin-media-query/src/index.js
@@ -0,0 +1,37 @@
+/* @flow */
+import { match } from 'css-mediaquery'
+import { Dimension } from 'react-native'
+import { isObject, isMediaQuery } from 'fela-utils'
+import assignStyle from 'css-in-js-utils/lib/assignStyle'
+
+type Orientation = 'landscape' | 'portrait'
+
+function getOrientation(width: number, height: number): Orientation {
+  return width > height ? 'landscape' : 'portrait'
+}
+
+function resolveMediaQuery(style: Object): Object {
+  const { width, height } = Dimension.get('window')
+
+  for (const property in style) {
+    const value = style[property]
+
+    if (
+      isMediaQuery(property) &&
+      isObject(value) &&
+      match(property, {
+        type: 'screen',
+        orientation: getOrientation(width, height),
+        width,
+        height
+      })
+    ) {
+      assignStyle(style, value)
+      delete style[property]
+    }
+  }
+
+  return style
+}
+
+export default () => resolveMediaQuery
