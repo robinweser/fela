11469b12aa1f6aeb791e9d0f8185ac2a38508275
Robin Frischmann <robin@rofrischmann.de>
2017-06-22 17:20:41 +0200

    fix flow

---
+++ packages/babel-plugin-fela/README.md
@@ -0,0 +1,3 @@
+# babel-plugin-fela
+
+> Experimental. Not shipped yet.
---
+++ packages/babel-plugin-fela/src/index.js
@@ -0,0 +1,16 @@
+/* @flow */
+type BabelInterface = {
+  types: Object
+}
+export default function ({ types }: BabelInterface) {
+  const { stringLiteral } = types
+
+  return {
+    visitor: {
+      Program(path: Object, file: any) {
+        path.body = stringLiteral('hello')
+        console.log(file)
+      }
+    }
+  }
+}
