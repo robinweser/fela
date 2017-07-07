9afcb3fa59004a295e0aff96bba5862091c69cfa
Mike Smart <mike@mikesmart.co.uk>
2017-07-07 16:22:52 +0200

    Fix human readable classname conflict

--- packages/fela-utils/src/__tests__/generateClassName-test.js
+++ packages/fela-utils/src/__tests__/generateClassName-test.js
@@ -4,8 +4,8 @@ describe('Generating a className', () => {
   it('should return a unique className', () => {
     expect(generateClassName(1)).toEqual('a')
     expect(generateClassName(2)).toEqual('b')
-    expect(generateClassName(53)).toEqual('bb')
-    expect(generateClassName(54)).toEqual('bc')
+    expect(generateClassName(53)).toEqual('b2')
+    expect(generateClassName(54)).toEqual('c2')
   })
 
   it('should return the same className', () => {
--- packages/fela-utils/src/generateClassName.js
+++ packages/fela-utils/src/generateClassName.js
@@ -1,18 +1,24 @@
 /* @flow  */
 const chars = 'abcdefghijklmnopqrstuvwxyz'
 const charLength = chars.length
+const charsSpecial = '1234567890_-'
+const charsSpecialLength = charsSpecial.length
 
 export default function generateClassName(
   id: number,
-  className: string = ''
+  className: string = '',
+  set: string = chars,
+  length: number = charLength
 ): string {
-  if (id <= charLength) {
-    return chars[id - 1] + className
+  if (id <= length) {
+    return className + set[id - 1]
   }
 
   // Bitwise floor as safari performs much faster https://jsperf.com/math-floor-vs-math-round-vs-parseint/55
   return generateClassName(
-    (id / charLength) | 0,
-    chars[id % charLength] + className
+    (id / length) | 0,
+    className + set[id % length],
+    charsSpecial,
+    charsSpecialLength
   )
 }
