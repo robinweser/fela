86cd99e9f02754718370b6aa91bb07c654d4fb3c
rofrischmann <robin@rofrischmann.de>
2017-07-26 09:24:26 +0200

    #327 allow array values for fontFace & animationName
    closes #327

--- packages/fela-plugin-embedded/src/__tests__/embedded-test.js
+++ packages/fela-plugin-embedded/src/__tests__/embedded-test.js
@@ -30,6 +30,47 @@ describe('Embedded plugin', () => {
     )
   })
 
+  it('should render inline multiple keyframes & fonts', () => {
+    const rule = () => ({
+      color: 'red',
+      animationName: [
+        {
+          '0%': { color: 'red' },
+          '100%': { color: 'blue' }
+        },
+        {
+          '0%': { backgroundColor: 'red' },
+          '100%': { backgroundColor: 'blue' }
+        }
+      ],
+      fontFace: [
+        {
+          fontFamily: 'Arial',
+          src: ['foo.svg', 'bar.ttf'],
+          fontWeight: 500
+        },
+        {
+          fontFamily: 'Lato Light',
+          src: ['baz.svg', 'asd.ttf'],
+          fontWeight: 400
+        }
+      ]
+    })
+
+    const renderer = createRenderer({ plugins: [embedded()] })
+    renderer.renderRule(rule)
+
+    expect(renderer.rules).toEqual(
+      '.a{color:red}.b{animation-name:k1,k2}.c{font-family:"Arial","Lato Light"}'
+    )
+    expect(renderer.keyframes).toEqual(
+      '@-webkit-keyframes k1{0%{color:red}100%{color:blue}}@-moz-keyframes k1{0%{color:red}100%{color:blue}}@keyframes k1{0%{color:red}100%{color:blue}}@-webkit-keyframes k2{0%{background-color:red}100%{background-color:blue}}@-moz-keyframes k2{0%{background-color:red}100%{background-color:blue}}@keyframes k2{0%{background-color:red}100%{background-color:blue}}'
+    )
+    expect(renderer.fontFaces).toEqual(
+      "@font-face{font-weight:500;src:url('foo.svg') format('svg'),url('bar.ttf') format('truetype');font-family:\"Arial\"}@font-face{font-weight:400;src:url('baz.svg') format('svg'),url('asd.ttf') format('truetype');font-family:\"Lato Light\"}"
+    )
+  })
+
   it('should render nested inline keyframes & fonts', () => {
     const rule = () => ({
       color: 'red',
