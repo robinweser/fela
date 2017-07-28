0191dae2b11b453119886e7a045764d4407dafaf
wcastand <wcastand@gmail.com>
2017-07-28 14:55:23 +0200

    add is property to the filter

--- packages/fela-utils/src/resolveUsedProps.js
+++ packages/fela-utils/src/resolveUsedProps.js
@@ -8,5 +8,5 @@ export default function resolveUsedProps(props: Array<string>, src: Object) {
       return output
     },
     []
-  ).filter(prop => prop !== 'innerRef')
+  ).filter(prop => prop !== 'innerRef' || prop !== 'is')
 }
