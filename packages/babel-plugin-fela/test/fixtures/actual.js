ba7e9374ceabef1bb1a003850f2155a9fe20ad1d
rofrischmann <robin@rofrischmann.de>
2017-07-29 11:50:38 +0200

    add first fixture

--- packages/babel-plugin-fela/test/fixtures/actual.js
+++ packages/babel-plugin-fela/test/fixtures/actual.js
@@ -1,1 +1,16 @@
-hiThere();
+import { createComponent } from 'react-fela'
+
+const rule = ({ fontSize, padding }) => ({
+  fontSize,
+  color: 'red',
+  '@media (min-height: 300px)': {
+    backgroundColor: 'red',
+    lineHeight: 2,
+    ':hover': {
+      color: 'black',
+      paddingLeft: padding
+    }
+  }
+})
+
+export default createComponent(rule)
--- packages/babel-plugin-fela/test/fixtures/expected.js
+++ packages/babel-plugin-fela/test/fixtures/expected.js
@@ -1,4 +1,29 @@
-"use strict";
+import { createComponent } from 'react-fela'
 
-"use helloworld";
-hiThere();
+const rule = ({ fontSize, padding }, renderer) => {
+  if (!renderer.cache._asdsd) {
+    renderer.cache._asdsd = renderer.renderRule(() => ({
+      color: 'red',
+      '@media (min-height: 300px)': {
+        backgroundColor: 'red',
+        lineHeight: 2,
+        ':hover': {
+          color: 'black'
+        }
+      }
+    }))
+  }
+
+  return {
+    _className: renderer.cache._asdsd,
+    fontSize,
+    color: 'red',
+    '@media (min-height: 300px)': {
+      ':hover': {
+        paddingLeft: padding
+      }
+    }
+  }
+}
+
+export default createComponent(rule)
