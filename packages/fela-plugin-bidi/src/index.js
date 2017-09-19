ec18b9801c729889d016f21b5bc2892281b44859
TxHawks <tofu.hawks@gmail.com>
2017-09-19 15:54:44 +0300

    remove unneeded type definitions in 'fela-plugin-bidi'

--- packages/fela-plugin-bidi/src/index.js
+++ packages/fela-plugin-bidi/src/index.js
@@ -1,11 +1,6 @@
 /* @flow */
 import transformStyle from 'bidi-css-js'
 
-import type DOMRenderer from '../../../flowtypes/DOMRenderer'
-import type NativeRenderer from '../../../flowtypes/NativeRenderer'
-
-type Type = 1 | 2 | 3 | 4 | 5
-
 export default function bidi(flowDirection: 'ltr' | 'rtl') {
   return (style: Object) => transformStyle(style, flowDirection)
 }
