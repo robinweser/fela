b82ef5ec0cf1894ba8dd10fe2a17a0cb10161142
Tim Swalling <tim@timswalling.com>
2017-10-02 08:08:56 +0100

    Add `createComponentWithProxy` to `preact-fela`

---
+++ packages/preact-fela/src/createComponentWithProxy.js
@@ -0,0 +1,7 @@
+/* @flow */
+import { h } from 'preact'
+import { createComponentFactory } from 'fela-bindings'
+
+import withTheme from './withTheme'
+
+export default createComponentFactory(h, withTheme, null, true)
