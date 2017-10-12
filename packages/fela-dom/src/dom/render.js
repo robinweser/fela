2df725ee46eb43dbf54aa7b8438b9b1f710fb39c
Robin Frischmann <robin.frischmann@dm.de>
2017-10-12 23:56:43 +0200

    remove noop warning

--- packages/fela-dom/src/dom/render.js
+++ packages/fela-dom/src/dom/render.js
@@ -5,14 +5,6 @@ import connectDOMNodes from './connectDOMNodes'
 import type DOMRenderer from '../../../../flowtypes/DOMRenderer'
 
 export default function render(renderer: DOMRenderer): void {
-  if (process.env.NODE_ENV !== 'production' && renderer.updateSubscription) {
-    console.warn(
-      'The renderer has already been rendered to the DOM.' +
-        'It listens to changes and automatically updates the StyleSheets.' +
-        'You should not call render() twice.'
-    )
-  }
-
   if (!renderer.updateSubscription) {
     connectDOMNodes(renderer)
 
