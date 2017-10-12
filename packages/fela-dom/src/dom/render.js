aa6ec73b987f65efbba68ef3f34c2f3e7897c423
Robin Frischmann <robin.frischmann@dm.de>
2017-10-12 23:53:58 +0200

    #412 do not subscribe to changes multiple times within render()
    closes #412

--- packages/fela-dom/src/dom/render.js
+++ packages/fela-dom/src/dom/render.js
@@ -5,11 +5,18 @@ import connectDOMNodes from './connectDOMNodes'
 import type DOMRenderer from '../../../../flowtypes/DOMRenderer'
 
 export default function render(renderer: DOMRenderer): void {
-  if (!renderer.isConnectedToDOM) {
-    renderer.isConnectedToDOM = true
-    connectDOMNodes(renderer)
+  if (process.env.NODE_ENV !== 'production' && renderer.updateSubscription) {
+    console.warn(
+      'The renderer has already been rendered to the DOM.' +
+        'It listens to changes and automatically updates the StyleSheets.' +
+        'You should not call render() twice.'
+    )
   }
 
-  const updateSubscription = createDOMSubscription(renderer.nodes)
-  renderer.subscribe(updateSubscription)
+  if (!renderer.updateSubscription) {
+    connectDOMNodes(renderer)
+
+    renderer.updateSubscription = createDOMSubscription(renderer.nodes)
+    renderer.subscribe(renderer.updateSubscription)
+  }
 }
