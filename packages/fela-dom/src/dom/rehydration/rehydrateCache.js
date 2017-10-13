074d06677cb2c72fbff6a015a51d3de484c32a48
Robin Frischmann <robin.frischmann@dm.de>
2017-10-13 23:20:35 +0200

    add types

--- packages/fela-dom/src/dom/rehydration/rehydrateCache.js
+++ packages/fela-dom/src/dom/rehydration/rehydrateCache.js
@@ -2,7 +2,10 @@
 import rehydrateSupportRules from './rehydrateSupportRules'
 import rehydrateRules from './rehydrateRules'
 
-export default function rehydrateCache(css, media = ''): Object {
+export default function rehydrateCache(
+  css: string,
+  media: string = ''
+): Object {
   const { ruleCss, supportCache } = rehydrateSupportRules(css, media)
 
   rehydrateRules(supportCache, ruleCss, media)
