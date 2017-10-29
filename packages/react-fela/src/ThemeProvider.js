e797adcdec91a5fe2e240e960a491bd2a940c3cd
rofrischmann <robin@rofrischmann.de>
2017-10-29 20:32:49 +0100

    update factory to 16

--- packages/react-fela/src/ThemeProvider.js
+++ packages/react-fela/src/ThemeProvider.js
@@ -1,24 +1,20 @@
 /* @flow */
-import { Component, Children } from 'react'
+import { Component } from 'react'
 import { ThemeProviderFactory } from 'fela-bindings'
 import PropTypes from 'prop-types'
 
-export default ThemeProviderFactory(
-  Component,
-  children => Children.only(children),
-  {
-    propTypes: {
-      theme: PropTypes.object.isRequired,
-      overwrite: PropTypes.bool
-    },
-    childContextTypes: {
-      theme: PropTypes.object
-    },
-    contextTypes: {
-      theme: PropTypes.object
-    },
-    defaultProps: {
-      overwrite: false
-    }
+export default ThemeProviderFactory(Component, children => children, {
+  propTypes: {
+    theme: PropTypes.object.isRequired,
+    overwrite: PropTypes.bool
+  },
+  childContextTypes: {
+    theme: PropTypes.object
+  },
+  contextTypes: {
+    theme: PropTypes.object
+  },
+  defaultProps: {
+    overwrite: false
   }
-)
+})
