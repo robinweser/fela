bbd2574178f18b76fd11897b14593d86be14b449
rofrischmann <robin@rofrischmann.de>
2017-11-14 15:20:34 +0100

    #468 add feature check

--- packages/fela-utils/src/extractUsedProps.js
+++ packages/fela-utils/src/extractUsedProps.js
@@ -2,6 +2,13 @@ export default function extractUsedProps(
   rule: Function,
   theme: Object = {}
 ): Array<string> {
+  // if the browser doesn't support proxies
+  // we simply return an empty props object
+  // see https://github.com/rofrischmann/fela/issues/468
+  if (typeof Proxy === 'undefined') {
+    return {}
+  }
+
   const handler = props => ({
     get(target, key) {
       if (typeof target[key] === 'object' && target[key] !== null) {
