da0934d7249458e766c3bb6f2278c29a00a23186
rofrischmann <robin@rofrischmann.de>
2017-11-15 21:18:09 +0100

    fix preset-dev tests

--- packages/fela-preset-dev/src/index.js
+++ packages/fela-preset-dev/src/index.js
@@ -3,11 +3,13 @@ import logger from 'fela-plugin-logger'
 import validator from 'fela-plugin-validator'
 
 type Config = {
-  alidator?: Array<any>
+  validator?: Array<any>
 }
 
-export const createDevPreset = (
+export function createDevPreset(
   { validator: validatorConfig = [] }: Config = {}
-) => [logger(), validator(...validatorConfig)]
+) {
+  return [logger(), validator(...validatorConfig)]
+}
 
 export default createDevPreset()
