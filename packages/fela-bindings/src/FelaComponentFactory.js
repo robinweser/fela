7236c25aaee2cb225aee4ee179b601b8428bb457
rofrischmann <robin@rofrischmann.de>
2018-01-18 18:03:38 +0100

    FelaTheme component

--- packages/fela-bindings/src/FelaComponentFactory.js
+++ packages/fela-bindings/src/FelaComponentFactory.js
@@ -2,34 +2,20 @@
 export default function FelaComponentFactory(
   BaseComponent: any,
   createElement: Function,
-  withTheme: Function,
+  FelaTheme: Function,
   contextTypes?: Object
-): any {
-  class FelaComponent extends BaseComponent {
-    constructor(props, context) {
-      super(props, context)
-
-      // we cache the rule and the component render method
-      // to reuse it later instead of creating a new one every time
-      this.component = props.render
-      this.rule = props.rule
-    }
-
-    render() {
-      const { render, rule, className, ...componentProps } = this.props
-      const { theme, renderer } = this.context
-
-      const felaClassName = renderer.renderRule(this.rule, {
-        ...componentProps,
-        theme,
-      })
-
-      return createElement(this.component, {
-        ...componentProps,
-        className: className ? className + ' ' + felaClassName : felaClassName,
-        theme,
-      })
-    }
+): Function {
+  function FelaComponent({ render, rule, ...otherProps }, { renderer }) {
+    return createElement(FelaTheme, {
+      render: theme =>
+        render({
+          className: renderer.renderRule(rule, {
+            ...otherProps,
+            theme,
+          }),
+          theme,
+        }),
+    })
   }
 
   if (contextTypes) {
