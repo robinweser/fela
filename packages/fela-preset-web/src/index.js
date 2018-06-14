8cc9ba05febc13b928bce5929ef9e3645a745576
rofrischmann <robin@rofrischmann.de>
2018-06-14 22:55:14 +0200

    remove LVHA plugin from fela-preset-web

--- packages/fela-preset-web/src/index.js
+++ packages/fela-preset-web/src/index.js
@@ -3,7 +3,6 @@ import extend from 'fela-plugin-extend'
 import embedded from 'fela-plugin-embedded'
 import prefixer from 'fela-plugin-prefixer'
 import fallbackValue from 'fela-plugin-fallback-value'
-import LVHA from 'fela-plugin-lvha'
 import unit from 'fela-plugin-unit'
 
 type Config = {
@@ -11,14 +10,14 @@ type Config = {
   unit?: Array<any>,
 }
 
-export const createWebPreset = (
-  { extend: extendConfig = [], unit: unitConfig = [] }: Config = {}
-) => [
+export const createWebPreset = ({
+  extend: extendConfig = [],
+  unit: unitConfig = [],
+}: Config = {}) => [
   extend(...extendConfig),
   embedded(),
   prefixer(),
   fallbackValue(),
-  LVHA(),
   unit(...unitConfig),
 ]
 
