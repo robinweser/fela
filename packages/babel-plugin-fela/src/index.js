4be12a07153b118f598f5cf8f0279f8a94b1c387
Robin Frischmann <robin@rofrischmann.de>
2018-06-14 23:30:07 +0200

    Update index.js

--- packages/babel-plugin-fela/src/index.js
+++ packages/babel-plugin-fela/src/index.js
@@ -3,4 +3,5 @@ import deprecate from './deprecate'
 
 deprecate(`The Fela Babel plugin (babel-plugin-fela) is deprecated, please remove it from your Babel configuration.
 It's use could lead to false output with newer Fela versions and doesn't add much value at all.`)
+
 export default createPlugin()
