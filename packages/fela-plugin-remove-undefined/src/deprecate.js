276d036145a5cbbb231290912fbb324102ee5fac
rofrischmann <robin@rofrischmann.de>
2018-07-05 20:01:13 +0200

    #583 removed deprecated packages

--- packages/fela-plugin-remove-undefined/src/deprecate.js
+++
@@ -1,8 +0,0 @@
-const cache = {}
-
-export default function deprecate(message) {
-  if (process.env.NODE_ENV !== 'production' && !cache[message]) {
-    console.warn(message)
-    cache[message] = true
-  }
-}
