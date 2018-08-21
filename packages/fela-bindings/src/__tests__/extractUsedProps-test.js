5560d292460610ebbd8e9ec3c71e9713589e6089
Alessandro Strada <alessandro.strada@gmail.com>
2018-08-21 18:38:19 +0200

    Fix test

--- packages/fela-bindings/src/__tests__/extractUsedProps-test.js
+++ packages/fela-bindings/src/__tests__/extractUsedProps-test.js
@@ -3,15 +3,16 @@ import extractUsedProps from '../extractUsedProps'
 describe('Extracting used props', () => {
   it('should return used props', () => {
     expect(
-      extractUsedProps(props => {
-        props.foo, props.bar
-      })
+      extractUsedProps(props => ({
+        foo: props.foo,
+        bar: props.bar,
+      }))
     ).toEqual(['foo', 'bar'])
   })
 
   it('should return an empty array if the rule throws an exception', () => {
     expect(
-      extractUsedProps(props => {
+      extractUsedProps(() => {
         throw new Error('test')
       })
     ).toEqual([])
