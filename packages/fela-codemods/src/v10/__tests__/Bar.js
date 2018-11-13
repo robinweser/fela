48c7f60585c947b85ab08e663772c0ca868cc93d
rofrischmann <robin.frischmann@dm.de>
2018-11-13 11:10:42 +0100

    remove test files

--- packages/fela-codemods/src/v10/__tests__/Bar.js
+++
@@ -1,9 +0,0 @@
-import { FelaTheme, FelaComponent, Provider } from 'react-fela'
-
-export default () => (
-  <Provider renderer="foo">
-    <FelaTheme
-      render={theme => <FelaComponent rule={({ foo }) => ({ color: foo })} />}
-    />
-  </Provider>
-)
--- packages/fela-codemods/src/v10/__tests__/Foo.js
+++
@@ -1,7 +0,0 @@
-import { FelaTheme, FelaComponent } from 'react-fela'
-
-export default () => (
-  <FelaTheme
-    render={theme => <FelaComponent rule={({ foo }) => ({ color: foo })} />}
-  />
-)
