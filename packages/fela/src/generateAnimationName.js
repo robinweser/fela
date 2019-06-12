6668ec387e30ead9677e4f58e79ea665b9df472f
Tiago Almeida <tjsa@ua.pt>
2019-06-12 10:46:45 -0400

    Update generateAnimationName.js

--- packages/fela/src/generateAnimationName.js
+++ packages/fela/src/generateAnimationName.js
@@ -1,4 +1,4 @@
 /* @flow */
-export default function generateAnimationName(id: number): string {
-  return 'k' + id
+export default function generateAnimationName(id: number, prefix?: string = ''): string {
+  return prefix + 'k' + id
 }
