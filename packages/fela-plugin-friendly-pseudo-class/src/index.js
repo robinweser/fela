a702535ad19a2f37f9c9211c0ddea9f49cc026f5
Philipp Achenbach <github@philipp-achenbach.de>
2019-11-15 10:44:30 +0100

    improvements in friendly-pseudo-class
    two improvements: firstly pseudo-elements are now supported (with '::' at the beginning, e.g. onAfter => '::after'), secondly the strings/properties are now better parsed (e.g. you had to use 'onFirst-of-type' for ':first-of-type' before, now onFirstOfType works as well).

--- packages/fela-plugin-friendly-pseudo-class/src/index.js
+++ packages/fela-plugin-friendly-pseudo-class/src/index.js
@@ -1,30 +1,45 @@
 /* @flow */
 import isPlainObject from 'isobject'
 
-const regex = new RegExp('^on([A-Z])')
+const regex = new RegExp('^on([A-Z])');
+const pseudoElements = [
+  'after',
+  'before',
+  'first-letter',
+  'first-line',
+  'selection',
+  'backdrop',
+  'placeholder',
+  'marker',
+  'spelling-error',
+  'grammar-error'
+];
 
 function friendlyPseudoClass(style: Object): Object {
   for (const property in style) {
-    const value = style[property]
+    const value = style[property];
 
     if (isPlainObject(value)) {
-      const resolvedValue = friendlyPseudoClass(value)
+      const resolvedValue = friendlyPseudoClass(value);
 
       if (regex.test(property)) {
-        const pseudo = property.replace(
-          regex,
-          (match, p1) => `:${p1.toLowerCase()}`
-        )
+        const pseudo = property
+          .replace(/([A-Z])/g, (match: string) => '-' + match.toLowerCase())
+          .replace(
+            /^on-(.*)/g,
+            (match, p1: string) =>
+              `${pseudoElements.includes(p1) ? '::' : ':'}${p1}`
+          );
 
-        style[pseudo] = resolvedValue
-        delete style[property]
+        style[pseudo] = resolvedValue;
+        delete style[property];
       } else {
-        style[property] = resolvedValue
+        style[property] = resolvedValue;
       }
     }
   }
 
-  return style
+  return style;
 }
 
 export default () => friendlyPseudoClass
