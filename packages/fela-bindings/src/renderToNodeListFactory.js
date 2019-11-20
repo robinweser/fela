12a52a77761e828d41da70ae0de355b4c8ab41b6
Daniel Stocks <daniel@webcloud.se>
2019-11-20 20:27:15 +0100

    fix flow type

--- packages/fela-bindings/src/renderToNodeListFactory.js
+++ packages/fela-bindings/src/renderToNodeListFactory.js
@@ -1,12 +1,10 @@
 /* @flow */
 import { renderToSheetList } from 'fela-dom'
-import type {
- DOMRenderer,
-} from '../../../flowtypes/DOMRenderer'
+import type { DOMRenderer } from '../../../flowtypes/DOMRenderer'
 
 export function renderToNodeListFactory(createElement: Function) {
-  return function renderToNodeList(renderer) {
-    const sheetList = renderToSheetList(renderer: DOMRenderer )
+  return function renderToNodeList(renderer: DOMRenderer) {
+    const sheetList = renderToSheetList(renderer)
 
     return sheetList.map(({ type, media, rehydration, support, css }) =>
       createElement('style', {
