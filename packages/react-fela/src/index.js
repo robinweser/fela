1a540b25958dbd714109351b1aa81cca38f04a40
Daniel Stocks <daniel@webcloud.se>
2019-11-22 11:20:05 +0100

    remove provider import

--- packages/react-fela/src/index.js
+++ packages/react-fela/src/index.js
@@ -4,7 +4,6 @@ import createComponent from './createComponent'
 import createComponentWithProxy from './createComponentWithProxy'
 import FelaComponent from './FelaComponent'
 import FelaTheme from './FelaTheme'
-import Provider from './Provider'
 import RendererProvider from './RendererProvider'
 import ThemeProvider from './ThemeProvider'
 import useFela from './useFela'
@@ -22,7 +21,6 @@ export {
   FelaComponent,
   FelaRenderer,
   FelaTheme,
-  Provider,
   RendererContext,
   RendererProvider,
   ThemeContext,
