4559711bcad5897381224dc0cf9af7e65739bbf9
Megan Smith <megan.smith@thetileapp.com>
2020-02-14 17:05:05 -0800

    IE 11 Support Error "Object doesn't support property or method 'findIndex'
    We use Fela on our site and it's breaking in Windows IE11 because, IE11 does not support findIndex.

--- packages/fela-utils/src/objectSortByScore.js
+++ packages/fela-utils/src/objectSortByScore.js
@@ -12,9 +12,14 @@ export default function objectSortByScore(
   const sortedKeys = objectReduce(
     obj,
     (resultSortedKeys, value, key) => {
-      const index = resultSortedKeys.findIndex(
-        el => getScore(obj[el], el) > getScore(value, key)
-      )
+      var index = -1;
+      for (var i = 0; i < resultSortedKeys.length; i++) {
+        var el = resultSortedKeys[i];
+        if (getScore(obj[el], el) > getScore(value, key)) {
+          index = i;
+          break;
+        }
+      }
 
       if (index !== -1) {
         return insertAtIndex(resultSortedKeys, key, index)
