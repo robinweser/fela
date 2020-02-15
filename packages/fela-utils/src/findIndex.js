61a5267d712f6aa48cab5bdd32a0862e144172cd
Megan Smith <megan@tokaypress.com>
2020-02-15 14:53:41 -0800

    findIndex

---
+++ packages/fela-utils/src/findIndex.js
@@ -0,0 +1,11 @@
+// workaround, because can't use native Array findIndex in IE https://github.com/robinweser/fela/pull/766
+export default function findIndex(array, predicate) {
+  var index = -1;
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
--- packages/fela-utils/src/objectSortByScore.js
+++ packages/fela-utils/src/objectSortByScore.js
@@ -1,8 +1,8 @@
 /* @flow */
 import arrayReduce from 'fast-loops/lib/arrayReduce'
-import objectReduce from 'fast-loops/lib/objectReduce'
-
+import findIndex from './findIndex';
 import insertAtIndex from './insertAtIndex'
+import objectReduce from 'fast-loops/lib/objectReduce'
 
 // TODO: we can further improve this one
 export default function objectSortByScore(
@@ -12,9 +12,9 @@ export default function objectSortByScore(
   const sortedKeys = objectReduce(
     obj,
     (resultSortedKeys, value, key) => {
-      const index = resultSortedKeys.findIndex(
-        el => getScore(obj[el], el) > getScore(value, key)
-      )
+      const index = findIndex(resultSortedKeys, (el) => {
+        return getScore(obj[el], el) > getScore(value, key);
+      });
 
       if (index !== -1) {
         return insertAtIndex(resultSortedKeys, key, index)
