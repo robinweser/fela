698ad5be811b2125cda59cf7afb1a76760072797
Robin Weser <contact@weser.io>
2020-04-16 09:34:40 +0200

    add a special check to allow null in url (due to base64)

--- packages/fela-utils/src/isUndefinedValue.js
+++ packages/fela-utils/src/isUndefinedValue.js
@@ -3,6 +3,8 @@ export default function isUndefinedValue(value: any): boolean {
   return (
     value === undefined ||
     value === null ||
-    (typeof value === 'string' && value.match(/(undefined|null)/) !== null)
+    (typeof value === 'string' &&
+      value.match(/(undefined|null)/) !== null &&
+      value.match(/url/) === null)
   )
 }
