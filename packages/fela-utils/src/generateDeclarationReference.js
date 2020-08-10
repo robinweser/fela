101e6326312fdd3d3b2d0bdb872f11cdca4eaf2a
Robin Weser <contact@weser.io>
2020-08-10 10:20:38 +0200

    remove camelCaseProperty to boost performance

--- packages/fela-utils/src/generateDeclarationReference.js
+++ packages/fela-utils/src/generateDeclarationReference.js
@@ -1,6 +1,4 @@
 /* @flow */
-import camelCaseProperty from 'css-in-js-utils/lib/camelCaseProperty'
-
 export default function generateDeclarationReference(
   property: string,
   value: any,
@@ -8,5 +6,5 @@ export default function generateDeclarationReference(
   media?: string = '',
   support?: string = ''
 ): string {
-  return support + media + pseudo + camelCaseProperty(property) + value
+  return support + media + pseudo + property + value
 }
