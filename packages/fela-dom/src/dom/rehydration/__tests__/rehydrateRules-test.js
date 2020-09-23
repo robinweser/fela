c1d409e5bd9ac6183f2370f954c9d8a64bcbd892
Ali Zeaiter <62549492+alizeait@users.noreply.github.com>
2020-09-23 16:23:53 +0200

    Add tests

--- packages/fela-dom/src/dom/rehydration/__tests__/rehydrateRules-test.js
+++ packages/fela-dom/src/dom/rehydration/__tests__/rehydrateRules-test.js
@@ -8,4 +8,15 @@ describe('Rehydrating rules', () => {
       )
     ).toMatchSnapshot()
   })
+  it('should rehydrate the renderer cache with specifity prefix', () => {
+    expect(
+      rehydrateRules(
+        '.parentClass .a{color:red}.parentClass .b{color:blue}.parentClass .c:hover:active{border-color:2px solid rgb(255, 255, 0)}',
+        '',
+        '',
+        {},
+        '.parentClass '
+      )
+    ).toMatchSnapshot()
+  })
 })
