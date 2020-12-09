8a7efd10d77a54bcc5051290c0ff1ee8d3ba8ad0
Robin Weser <contact@weser.io>
2020-12-09 11:56:26 +0100

    remove devMode-specific insertRule, closes #817 (#825)

--- packages/fela-dom/src/dom/connection/__tests__/createSubscription-test.js
+++ packages/fela-dom/src/dom/connection/__tests__/createSubscription-test.js
@@ -1,5 +1,3 @@
-import { html as beautify } from 'js-beautify'
-
 import createSubscription from '../createSubscription'
 
 import createRenderer from '../../../../../fela/src/createRenderer'
@@ -14,7 +12,7 @@ afterEach(() => {
 
 describe('Subscribing to the DOM', () => {
   it('should render rules to a DOM node', () => {
-    const renderer = createRenderer({ devMode: true })
+    const renderer = createRenderer()
 
     const updateSubscription = createSubscription(renderer)
     renderer.subscribe(updateSubscription)
@@ -27,11 +25,11 @@ describe('Subscribing to the DOM', () => {
       },
     }))
 
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
+    const styleSheets = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    expect(styleSheets).toMatchSnapshot()
   })
 
   it('should render static styles to a DOM node', () => {
@@ -48,11 +46,11 @@ describe('Subscribing to the DOM', () => {
       'body, html'
     )
 
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
+    const styleSheets = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    expect(styleSheets).toMatchSnapshot()
   })
 
   it('should render keyframes to a DOM node', () => {
@@ -66,121 +64,14 @@ describe('Subscribing to the DOM', () => {
       to: { color: 'blue' },
     }))
 
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
+    const styleSheets = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    expect(styleSheets).toMatchSnapshot()
   })
 
   it('should render media rules and support rules to single DOM nodes (devMode)', () => {
-    const renderer = createRenderer({ devMode: true })
-
-    const updateSubscription = createSubscription(renderer)
-    renderer.subscribe(updateSubscription)
-
-    renderer.renderRule(() => ({
-      color: 'blue',
-      '@supports (display: flex)': {
-        display: 'flex',
-      },
-      '@media (min-width: 300px)': {
-        color: 'red',
-        '@media (max-height: 500px)': {
-          color: 'yellow',
-          '@supports (display: grid)': {
-            display: 'grid',
-          },
-        },
-      },
-    }))
-
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
-  })
-
-  it('should clear all DOM nodes', () => {
-    const renderer = createRenderer({ devMode: true })
-
-    const updateSubscription = createSubscription(renderer)
-    renderer.subscribe(updateSubscription)
-
-    renderer.renderRule(() => ({
-      color: 'blue',
-      '@media (min-width: 300px)': {
-        color: 'red',
-        '@media (max-height: 500px)': {
-          color: 'yellow',
-        },
-      },
-    }))
-
-    renderer.renderKeyframe(() => ({
-      from: { color: 'red' },
-      to: { color: 'blue' },
-    }))
-
-    renderer.clear()
-
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
-  })
-
-  it('should correctly recreate nodes after clean', () => {
-    const renderer = createRenderer({ devMode: true })
-
-    const updateSubscription = createSubscription(renderer)
-    renderer.subscribe(updateSubscription)
-
-    renderer.renderRule(() => ({
-      color: 'blue',
-      '@media (min-width: 300px)': {
-        color: 'red',
-        '@media (max-height: 500px)': {
-          color: 'yellow',
-        },
-      },
-    }))
-
-    renderer.renderKeyframe(() => ({
-      from: { color: 'red' },
-      to: { color: 'blue' },
-    }))
-
-    const beforeClean = document.head.outerHTML
-
-    renderer.clear()
-
-    renderer.renderRule(() => ({
-      color: 'blue',
-      '@media (min-width: 300px)': {
-        color: 'red',
-        '@media (max-height: 500px)': {
-          color: 'yellow',
-        },
-      },
-    }))
-
-    renderer.renderKeyframe(() => ({
-      from: { color: 'red' },
-      to: { color: 'blue' },
-    }))
-
-    expect(beforeClean === document.head.outerHTML)
-    expect(
-      beautify(document.head.outerHTML, {
-        indent_size: 2,
-      })
-    ).toMatchSnapshot()
-  })
-
-  it('should use insertRule in production', () => {
     const renderer = createRenderer()
 
     const updateSubscription = createSubscription(renderer)
@@ -202,6 +93,80 @@ describe('Subscribing to the DOM', () => {
       },
     }))
 
+    const styleSheets = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    expect(styleSheets).toMatchSnapshot()
+  })
+
+  it('should clear all DOM nodes', () => {
+    const renderer = createRenderer()
+
+    const updateSubscription = createSubscription(renderer)
+    renderer.subscribe(updateSubscription)
+
+    renderer.renderRule(() => ({
+      color: 'blue',
+      '@media (min-width: 300px)': {
+        color: 'red',
+        '@media (max-height: 500px)': {
+          color: 'yellow',
+        },
+      },
+    }))
+
+    renderer.renderKeyframe(() => ({
+      from: { color: 'red' },
+      to: { color: 'blue' },
+    }))
+
+    renderer.clear()
+
+    const styleSheets = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    expect(styleSheets).toMatchSnapshot()
+  })
+
+  it('should correctly recreate nodes after clean', () => {
+    const renderer = createRenderer()
+
+    const updateSubscription = createSubscription(renderer)
+    renderer.subscribe(updateSubscription)
+
+    renderer.renderRule(() => ({
+      color: 'blue',
+      '@media (min-width: 300px)': {
+        color: 'red',
+        '@media (max-height: 500px)': {
+          color: 'yellow',
+        },
+      },
+    }))
+
+    renderer.renderKeyframe(() => ({
+      from: { color: 'red' },
+      to: { color: 'blue' },
+    }))
+
+    const styleSheetsBeforeClean = Object.keys(renderer.nodes).map((key) =>
+      getStyleSheetStyle(renderer.nodes[key])
+    )
+
+    renderer.clear()
+
+    renderer.renderRule(() => ({
+      color: 'blue',
+      '@media (min-width: 300px)': {
+        color: 'red',
+        '@media (max-height: 500px)': {
+          color: 'yellow',
+        },
+      },
+    }))
+
     renderer.renderKeyframe(() => ({
       from: { color: 'red' },
       to: { color: 'blue' },
@@ -211,6 +176,7 @@ describe('Subscribing to the DOM', () => {
       getStyleSheetStyle(renderer.nodes[key])
     )
 
+    expect(styleSheetsBeforeClean).toEqual(styleSheets)
     expect(styleSheets).toMatchSnapshot()
   })
 })
