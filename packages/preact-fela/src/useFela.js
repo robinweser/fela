34f1cb1b04857350e45f06351ae22bf7fbf26da7
pontakornth <pontakorn61@gmail.com>
2020-12-29 16:27:53 +0700

    feat: Add useFela hook

---
+++ packages/preact-fela/src/useFela.js
@@ -0,0 +1,38 @@
+/* @flow */
+// $FlowFixMe
+import { useContext } from 'preact/hooks'
+import { combineRules } from 'fela'
+
+import { RendererContext, ThemeContext } from './context'
+
+type HookInterface = {
+  css: Function,
+  theme: Object,
+  renderer: Object,
+}
+
+export default function useFela(props: Object = {}): HookInterface {
+  const renderer = useContext(RendererContext)
+  const theme = useContext(ThemeContext) || {}
+
+  if (!renderer) {
+    throw new Error(
+      'The "useFela" hook can only be used  inside a "RendererProvider"'
+    )
+  }
+
+  const propsWithTheme = {
+    ...props,
+    theme,
+  }
+
+  function css(...rules: Array<Object | Function>) {
+    return renderer.renderRule(combineRules(...rules), propsWithTheme)
+  }
+
+  return {
+    renderer,
+    theme,
+    css,
+  }
+}
