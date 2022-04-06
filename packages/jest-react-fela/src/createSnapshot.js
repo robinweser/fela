34c3537f36c6a2fc3be65c737ed3f7471b70e7fe
Robin Weser <contact@weser.io>
2022-04-06 10:39:29 +0200

    use createRoot for snapshots when available

--- packages/jest-react-fela/src/createSnapshot.js
+++ packages/jest-react-fela/src/createSnapshot.js
@@ -5,9 +5,29 @@ import { RendererProvider, ThemeProvider } from 'react-fela'
 
 import { createSnapshotFactory } from 'jest-fela-bindings'
 
+let createRoot
+try {
+  const ReactDOMClient = require('react-dom/client')
+  if (
+    typeof ReactDOMClient !== 'undefined' &&
+    typeof ReactDOMClient.createRoot !== 'undefined'
+  ) {
+    createRoot = ReactDOMClient.createRoot
+  }
+} catch (e) {}
+
+function renderComponent(component, node) {
+  if (createRoot) {
+    const root = ReactDOMClient.createRoot(component)
+    root.render(node)
+  } else {
+    render(component, node)
+  }
+}
+
 export default createSnapshotFactory(
   createElement,
-  render,
+  renderComponent,
   createRenderer(),
   RendererProvider,
   ThemeProvider
